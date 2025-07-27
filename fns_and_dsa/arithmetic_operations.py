16c09cac18486ada4643a0a69a6d22d0db861529
Cwills <ampocoll.wills@gmail.com>
2025-07-26 19:05:45 -0800

    add arithmetic_operation

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+def perform_operation(num1, num2, operation):
+    result = 0
+    if operation == 'add':
+        result = num1 + num2
+    elif operation == 'subtract':
+        result = num1 - num2
+    elif operation == 'multiply':
+        result = num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            result = 'Cannot divide by 0'
+        else:
+            result = num1 / num2
+
+    return result
