c99247cd9f9446169288916652c6b62560a76378
Cwills <ampocoll.wills@gmail.com>
2025-07-31 15:47:10 -0800

    Add add bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,14 +5,14 @@ class BankAccount:
     def deposit(self, amount):
         if amount > 0:
             self.account_balance += amount
-            print(f"Deposited: ${amount:.1f}")
+            print(f"Deposited: ${amount}")
         else:
             print("Deposit amount must be positive.")
 
     def withdraw(self, amount):
         if amount <= self.account_balance:
             self.account_balance -= amount
-            print(f"Withdrew: ${amount:.1f}")
+            print(f"Withdrew: ${amount}")
             return True
         else:
             print("Insufficient funds.")
